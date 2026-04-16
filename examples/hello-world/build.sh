e17ff996b3f6b785a879b36bce3ea01884ef1782
Eva van Houten <opensource@evavh.net>
2026-04-16 11:49:54 +0200

    Fix build, now runs on emulator!!!

--- examples/hello-world/build.sh
+++ examples/hello-world/build.sh
@@ -6,6 +6,8 @@ cargo clean
 
 target="thumbv7m-none-eabi"
 
+export RUSTFLAGS="-C relocation-model=pie -C codegen-units=1 -C link-arg=--gc-sections -C link-arg=--build-id=sha1 -C link-arg=--emit-relocs -C debuginfo=2"
+
 # Build the project through Cargo
 cargo --version
 cargo build --target $target --release || exit 1
